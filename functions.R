231fd8303410a247a1a1cdba9bbdff274b8d89b8
Eligijus Bujokas <elasorama@gmail.com>
2017-01-22 23:04:56 +0200

    Pie chart
    Added a function for a pie chart for injuries and fatalities.

--- functions.R
+++ functions.R
@@ -1,14 +1,14 @@
 download.terror <- function(years){
-
+  
   for(year in years){
     cat('downloading year...', year, "\n")
     url <- paste0('http://www.start.umd.edu/gtd/search/ResultsCSV.aspx?csv=1&casualties_type=&casualties_max=&start_year=', 
                   year, '&start_month=1&start_day=1&end_year=', year, '&end_month=12&end_day=31')
     download.file(url, paste0("data/raw data", year, ".csv"), "internal", quiet = FALSE, mode = "w",
-                              cacheOK = TRUE,
-                              extra = getOption("download.file.extra"))
+                  cacheOK = TRUE,
+                  extra = getOption("download.file.extra"))
   }
-
+  
 }
 
 read.terror <- function(path){
@@ -22,6 +22,7 @@ read.terror <- function(path){
   
   ## Formating in a nice way
   raw.data[raw.data==""] <- NA
+  raw.data[raw.data=="<NA>"] <- NA
   na.index <- which(apply(raw.data, 1, function(x) all(is.na(x))))
   if(length(na.index)!=0) raw.data <- raw.data[-na.index, ]
   raw.data <- raw.data[, c("DATE", "COUNTRY", "CITY", "PERPETRATOR.1", "FATALITIES", "INJURED", "TARGET.TYPE.1", "ATTACK.TYPE.1",
@@ -36,5 +37,53 @@ read.terror <- function(path){
   return(raw.data)
 }
 
+legendary2 <- function (legend, col, line = 0, side = 3, adj = 1, cex = 0.8, 
+                        presymbol = " - ") 
+{
+  legend <- paste(presymbol, legend)
+  olegend <- legend
+  for (i in 1:length(olegend)) {
+    xlegend <- legend
+    xlegend <- paste0("'", legend, "'")
+    xlegend[-i] <- paste0("phantom(`", xlegend[-i], "`)")
+    xlegend <- paste0(xlegend, collapse = " * ")
+    xlegend <- paste0("expression(", xlegend, ")")
+    xlegend <- paste0("mtext(", xlegend, ",col = '", col[i], 
+                      "',line = ", line, ",side = ", side, ",adj = ", adj, 
+                      ",cex = ", cex, ")")
+    eval(parse(text = xlegend))
+  }
+}
+
+pie.chart <- function(data, slices, cols=NULL, title){
+  data <- data[!is.na(data)]
+  for.pie <- numeric()
+  
+  for(j in 2:length(slices)){
+    for.pie <- c(for.pie, length(data[which(data>=slices[j - 1] & data<slices[j])])/length(data))
+  }  
+  
+  for.pie <- c(for.pie, length(data[which(data>=slices[j])])/length(data))
+  
+  if(is.null(cols)) cols <- 1:length(slices)
+  
+  label <- character()
+  
+  for(j in 1:length(slices)){
+    if(j == length(slices)){
+      label <- c(label, paste0("More than ", slices[j])) 
+    } else {
+      if(j == 1) {
+        label <- c(label, paste0("Equal to ", slices[j]))
+      } else {
+        label <- c(label, paste0("Between ", slices[j], " and ", slices[j + 1]))
+      }
+    }
+  }
+  
+  pie3D(x = for.pie, col=cols, labels= label, main = title, labelcex = 1.2, explode = 0.1, radius=.9, 
+        labelrad=1.4)
+  legendary2(round(for.pie*100, 1), cols, cex=1)
+}
 
 
